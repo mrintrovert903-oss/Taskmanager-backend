2c34dc813cea07e97201e9f5adae12fdc91ee083
Alwayshari1 <gupteswarraohari0000@gmail.com>
2025-10-31 14:06:25 +0530

    task backend

--- index.js
+++ index.js
@@ -10,6 +10,24 @@ dotenv.config()
 const app = express();
 app.use(express.json());
 app.use(cors());
+const allowedOrigins = [
+  "http://localhost:3000",                    // local React app
+  "https://task-manager-roan-nine-13.vercel.app/"     // your deployed Vercel URL
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+  })
+);
 
 app.get('/',(req,res)=>{
     res.send('Task Manager API is running...')
